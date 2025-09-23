0e78ccfb39f0fdb39affebb1df603316f105ca58
Matheus Vicente <vicentematheus1507@gmail.com>
2025-09-23 16:57:12 -0300

    chore: add additional imports for TouchableOpacity, TextInput, ScrollView, and Alert in App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView, Alert } from 'react-native';
 import { useState, useEffect } from 'react';
+
 
 export default function App() {
   return (

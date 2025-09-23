5ca36c2d1fae3bebc4f49e41ceed855b8d07eebc
Matheus Vicente <vicentematheus1507@gmail.com>
2025-09-23 16:57:22 -0300

    chore: replace View with SafeAreaView for better layout handling

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView, Alert } from 'react-native';
 import { useState, useEffect } from 'react';
-
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 

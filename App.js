e48d4335a42d5f0604891d99a0c92003734e8eb7
Matheus Vicente <vicentematheus1507@gmail.com>
2025-09-23 16:23:07 -0300

    chore: import useState and useEffect hooks in App.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
 
 export default function App() {
   return (

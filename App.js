fbca5b44bd6b93e5159777be70397d6777a6ef39
Matheus Vicente <vicentematheus1507@gmail.com>
2025-10-01 23:59:14 -0300

    chore: implement saving updated reminders list to AsyncStorage in handleAddLembrete function

--- App.js
+++ App.js
@@ -45,18 +45,9 @@ export default function App() {
   setLembretes(listaAtualizada)
 }
 // 4. SALVA a lista atualizada no AsyncStorage.
-  try {
-    await AsyncStorage.setItem("@lembretes", JSON.stringify(listaAtualizada));
-  } catch (e) {
-      console.error("Falha ao salvar o lembrete.", e);
-  }
-  };
+ 
 
-  // 5. Limpa o campo do formulário e volta para a tela de lista.
-    setTextoLembrete('');
-    setView('lista');
-  };
-
+ 
   return (
     <SafeAreaView style={styles.container}>
       
